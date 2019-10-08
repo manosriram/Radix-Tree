2de64e5b5699399c8161ca924dffb2d3da6ab15d
Mano Sriram <mano.sriram0@gmail.com>
2019-10-08 14:38:03 +0530

    Add Basic Functionalities.

--- incALL.hpp
+++ incALL.hpp
@@ -1,10 +1,10 @@
 #ifndef INC_ALL
 #define INC_ALL
 
-#include "includes/RadixIncludes/baseInclude.hpp"
-#include "includes/RadixIncludes/cutPosition.hpp"
-#include "includes/RadixIncludes/split.hpp"
-#include "includes/RadixIncludes/traverseTree.hpp"
-#include "includes/RadixIncludes/insertWord.hpp"
+#include "includes/baseInclude.hpp"
+#include "includes/cutPosition.hpp"
+#include "includes/split.hpp"
+#include "includes/traverseTree.hpp"
+#include "includes/insertWord.hpp"
 
 #endif
